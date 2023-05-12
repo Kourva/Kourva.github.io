d23b8f15a215fbda32a6eeae4a31622c5fd5fbc9
Kourva <118578799+Kourva@users.noreply.github.com>
2023-05-12 09:09:33 +0330

    Update rle.py

--- Scripts/rle.py
+++ Scripts/rle.py
@@ -2,51 +2,56 @@ from pyscript import Element
     
 # Encode data
 def encode():
-    encode_input = Element("encode_input").value  # Input
-    encode_output = Element("encode_output")      # Output
+    try:
+        encode_input = Element("encode_input").value  # Input
+        encode_output = Element("encode_output")      # Output
 
-    if encode_input == "":
-        encode_output.write("Input is required for decoding!")
-        return
+        if encode_input == "":
+            encode_output.write("Input is required for decoding!")
+            return
 
-    # RLE Compression
-    data = encode_input
-    compressed_data = ""
-    count = 1
-    for i in range(1, len(data)):
-        if data[i] == data[i - 1]:
-            count += 1
-        else:
-            compressed_data += str(count) + data[i - 1]
-            count = 1
-    compressed_data += str(count) + data[-1]
+        # RLE Compression
+        data = encode_input
+        compressed_data = ""
+        count = 1
+        for i in range(1, len(data)):
+            if data[i] == data[i - 1]:
+                count += 1
+            else:
+                compressed_data += str(count) + data[i - 1]
+                count = 1
+        compressed_data += str(count) + data[-1]
 
-    # Set Output
-    encode_output.write(compressed_data) 
+        # Set Output
+        encode_output.write(compressed_data)
+    except Exception as ex:
+        Element("encode_output").write(f"[!] Error\n\n{ex}")
 
 # Decode data
 def decode():
-    decode_input = Element("decode_input").value  # Input
-    decode_output = Element("decode_output")      # Output
+    try:
+        decode_input = Element("decode_input").value  # Input
+        decode_output = Element("decode_output")      # Output
 
-    if decode_input == "":
-        decode_output.write("Input is required for decoding!")
-        return
+        if decode_input == "":
+            decode_output.write("Input is required for decoding!")
+            return
 
-    # RLE De-Compression
-    data = decode_input
-    decompressed_data = ""
-    count = ""
-    for character in data:
-        if character.isdigit():
-            count += character
-        else:
-            decompressed_data += int(count) * character
-            count = ""
+        # RLE De-Compression
+        data = decode_input
+        decompressed_data = ""
+        count = ""
+        for character in data:
+            if character.isdigit():
+                count += character
+            else:
+                decompressed_data += int(count) * character
+                count = ""
 
-    # Set Output
-    decode_output.write(decompressed_data) 
-
+        # Set Output
+        decode_output.write(decompressed_data) 
+    except Exception as ex:
+        Element("decode_output").write(f"[!] Error\n\n{ex}")
 
 # Reset Encode Output
 def en_reset():
