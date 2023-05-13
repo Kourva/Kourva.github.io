6814b9bca06c9e5c31a033b49fd827f19d87aba9
Kourva <118578799+Kourva@users.noreply.github.com>
2023-05-13 06:43:16 +0330

    Update iplookup.py

--- IPLookup/Scripts/iplookup.py
+++ IPLookup/Scripts/iplookup.py
@@ -8,13 +8,15 @@ async def lookup():
         target = Element("addrs-input").value
         output = Element("lookup_output")
         
-        response = await request(f"http://ipwho.is/{target}", method="GET")
+        response = await request(f"https://freeipapi.com/api/json/{target}", method="GET")
         output2 = await response.json()
         lat, lon = output2['latitude'], output2['longitude']
         
         
         outmap = Element("location")
-        outmap.element.src = f"https://maps.google.com/maps?q={lat},{lon}&hl=es;z=14&output=embed"
+        baseurl = f"https://maps.google.com/maps?q={lat},{lon}&hl=es;z=14&output=embed"
+        if outmap.element.src != baseurl:
+            outmap.element.src = baseurl
         
         output.write((json.dumps(output2, indent=4)))
     except:
