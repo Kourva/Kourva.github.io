0359c414bd107bc4e3e0313e7b7af0465ded8041
Kourva <118578799+Kourva@users.noreply.github.com>
2024-06-21 03:12:26 +0330

    Update main.py

--- Clicker/script/main.py
+++ Clicker/script/main.py
@@ -3,15 +3,8 @@ from pyscript import window, document, display
 from js import navigator
 from pyscript import sync
 
-# Bootstrap the Pyodide worker, with optional config too.
-# The worker is:
-#   * Owned by this script, no JS or Pyodide code in the same page can access
-#     it.
-#   * It allows pre-sync methods to be exposed.
-#   * It has a ready Promise to await for when Pyodide is ready in the worker. 
-#   * It allows the use of post-sync (methods exposed by Pyodide in the
-#     worker).
-worker = PyWorker("./script/main.py", type="pyodide")
+# Bootstrap the MicroPython worker.
+worker = PyWorker("./script/main.py", type="micropython")
 
 # wait until Pyodide has completed its bootstrap, and is ready.
 worker.ready
@@ -23,14 +16,10 @@ document.querySelector("#main_loader").remove()
 # Free memory and get rid of everything in the worker.
 worker.terminate()
 
-user_detail = dir(window.Telegram)
-
 user_data = document.createElement("div");
 user_data.appendChild(
     document.createTextNode(
-        f"User Data:\n{user_detail}"
+        f"User Data:\n{window.Telegram}"
     )
 )
-
 document.body.append(user_data)
-
