1257f46f731a7a7bd508322081e13080f69f176c
Kourva <118578799+Kourva@users.noreply.github.com>
2024-06-21 02:00:27 +0330

    Delete Clicker/click.js

--- Clicker/click.js
+++
@@ -1,6 +0,0 @@
-function Click() {
-  balance += (1000000000 * multiplier);
-  updateCount();
-  document.getElementById('collectmoney').hidden = true;
-  document.getElementById('collectmoney').hidden = false;
-}
