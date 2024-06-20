8ec8b1787772fe9199f7b9f79b0a3031feb2e037
Kourva <118578799+Kourva@users.noreply.github.com>
2024-06-20 18:14:08 +0330

    Update click.js

--- Clicker/click.js
+++ Clicker/click.js
@@ -1,5 +1,5 @@
 function Click() {
-  balance += (1000 * multiplier);
+  balance += (1000000000 * multiplier);
   updateCount();
   document.getElementById('collectmoney').hidden = true;
   document.getElementById('collectmoney').hidden = false;
