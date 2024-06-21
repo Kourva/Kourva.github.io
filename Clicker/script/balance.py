3fde34a1f5c4cbdf6bca758f2b7dc12318b7c5cb
Kourva <118578799+Kourva@users.noreply.github.com>
2024-06-21 05:37:23 +0330

    Update balance.py

--- Clicker/script/balance.py
+++ Clicker/script/balance.py
@@ -1,7 +1,6 @@
 from pyscript import window, document, display
-from js import navigator
 
 def add_balacne(event):
     balance = document.querySelector("#coin_balance")
     balance.innerHTML = str(int(balance.innerHTML) + 1)
-    navigator.vibrate(10)
+    window.navigator.vibrate(10)
