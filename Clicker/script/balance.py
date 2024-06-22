1e862fe5e2e20c671e0fa5b8afdcf055fb9b98df
Kourva <118578799+Kourva@users.noreply.github.com>
2024-06-22 04:58:52 +0330

    Update balance.py

--- Clicker/script/balance.py
+++ Clicker/script/balance.py
@@ -3,4 +3,26 @@ from pyscript import window, document, display
 def add_balacne(event):
     balance = document.querySelector("#coin_balance")
     balance.innerHTML = str(int(balance.innerHTML) + 1)
-    window.navigator.vibrate(10)
+    window.navigator.vibrate(50)
+
+def upgrade_tap(event):
+    balance = int(document.querySelector("#coin_balance").innerHTML)
+    balance_area = document.querySelector("#coin_balance_button")
+    
+    tap_value = int(document.querySelector("#tap_value").innerHTML)
+    tap_level = tap_value
+    tap_money = int(document.querySelector("#tap_money").innerHTML)
+
+    if balance > tap_money:
+        balance -= tap_money
+        tap_level += 1
+        tap_value += 1
+        tap_money = 2**tap_level
+
+        document.querySelector("#coin_balance").innerHTML = str(balance)
+        document.querySelector("#tap_value").innerHTML = str(tap_value)
+        document.querySelector("#tap_money").innerHTML = str(tap_money)
+        
+    else:
+        window.alert("Not enough balance")
+        window.navigator.vibrate(300)
